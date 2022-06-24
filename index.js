3ca0b4770179b1adc0d757f30e3d400e53346aa8
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-24 12:42:32 +0300

    add render specific country covid information

--- index.js
+++ index.js
@@ -25,37 +25,54 @@ renderCountry(county) {
                     list.innerText = county.country;
                     countryList.appendChild(list);
 
-                }
-                //like section
 
-                let integer = 0;
+                    //render spefic country details
 
-                likeButton.addEventListener('click', () => {
-                    integer += 1
-                    countLikes.innerHTML = `${integer} likes`;
-                })
-                //add comment
-                document.addEventListener('DOMContentLoaded', () => {
-                    let form = document.querySelector('#comment_form')
-                    form.addEventListener('submit', (event) => {
-                        event.preventDefault()
-                        handlePost(event.target['comment_input'].value)
-                        form.reset()
+                    list.addEventListener('click', () => {
+
+
+                        countryName.textContent = county.Country;
+                        countryCode.textContent = county.CountryCode;
+                        date.textContent = county.Date;
+                        confirmed.textContent = county.NewConfirmed;
+                        deaths.textContent = county.TotalDeaths;
+                        recovered.textContent = recovered.TotalRecovered;
                     })
 
-                })
-
-                function handlePost(post) {
-
-                    let postt = document.createElement('comment_review')
-                    let btn = document.createElement('buton')
-                    btn.addEventListener('click', (event) =>
-                        event.target.parentNode.remove()
-
-                    )
-
-                    postt.textContent = `   ${post}`
-
-                    document.querySelector('#comments_list').appendChild(postt)
-
                 }
+
+
+            }
+            //like section
+
+        let integer = 0;
+
+        likeButton.addEventListener('click', () => {
+                integer += 1
+                countLikes.innerHTML = `${integer} likes`;
+            })
+            //add comment
+        document.addEventListener('DOMContentLoaded', () => {
+            let form = document.querySelector('#comment_form')
+            form.addEventListener('submit', (event) => {
+                event.preventDefault()
+                handlePost(event.target['comment_input'].value)
+                form.reset()
+            })
+
+        })
+
+        function handlePost(post) {
+
+            let postt = document.createElement('comment_review')
+            let btn = document.createElement('buton')
+            btn.addEventListener('click', (event) =>
+                event.target.parentNode.remove()
+
+            )
+
+            postt.textContent = `   ${post}`
+
+            document.querySelector('#comments_list').appendChild(postt)
+
+        }

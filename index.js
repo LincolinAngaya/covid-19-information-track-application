6d25a3bb99c9d5c943909bbd778bb21d3a7050f6
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-24 12:22:46 +0300

    re-arrange the variables

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//declare variables
+const countryList = document.querySelector('#countries');
+const countryName = document.getElementById('country');
+const countryCode = document.getElementById('code');
+const date = document.getElementById('date');
+const confirmed = document.getElementById('confirmed');
+const deaths = document.getElementById('deaths');
+const recovered = document.getElementById('recovered');
+let countLikes = document.getElementById('like-count')
+let likeButton = document.getElementById('like-button')
+    //add fectch section
 document.addEventListener("DOMContentLoaded", () => {
     fetchCountry();
 });
@@ -9,49 +20,42 @@ function fetchCountry() {
         .then((movies) => renderCountry(county))
 }
 renderCountry(county) {
+        county.forEach(county => {
+                    let list = document.createElement('li');
+                    list.innerText = county.country;
+                    countryList.appendChild(list);
 
+                }
+                //like section
 
-    }
-    //declare variables
-const countryList = document.querySelector('#countries');
-const countryName = document.getElementById('country');
-const countryCode = document.getElementById('code');
-const date = document.getElementById('date');
-const confirmed = document.getElementById('confirmed');
-const deaths = document.getElementById('deaths');
-const recovered = document.getElementById('recovered');
-let countLikes = document.getElementById('like-count')
-let likeButton = document.getElementById('like-button')
+                let integer = 0;
 
+                likeButton.addEventListener('click', () => {
+                    integer += 1
+                    countLikes.innerHTML = `${integer} likes`;
+                })
+                //add comment
+                document.addEventListener('DOMContentLoaded', () => {
+                    let form = document.querySelector('#comment_form')
+                    form.addEventListener('submit', (event) => {
+                        event.preventDefault()
+                        handlePost(event.target['comment_input'].value)
+                        form.reset()
+                    })
 
-let integer = 0;
+                })
 
-likeButton.addEventListener('click', () => {
-        integer += 1
-        countLikes.innerHTML = `${integer} likes`;
-    })
-    //add comment
-document.addEventListener('DOMContentLoaded', () => {
-    let form = document.querySelector('#comment_form')
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        handlePost(event.target['comment_input'].value)
-        form.reset()
-    })
+                function handlePost(post) {
 
-})
+                    let postt = document.createElement('comment_review')
+                    let btn = document.createElement('buton')
+                    btn.addEventListener('click', (event) =>
+                        event.target.parentNode.remove()
 
-function handlePost(post) {
+                    )
 
-    let postt = document.createElement('comment_review')
-    let btn = document.createElement('buton')
-    btn.addEventListener('click', (event) =>
-        event.target.parentNode.remove()
+                    postt.textContent = `   ${post}`
 
-    )
+                    document.querySelector('#comments_list').appendChild(postt)
 
-    postt.textContent = `   ${post}`
-
-    document.querySelector('#comments_list').appendChild(postt)
-
-}
+                }

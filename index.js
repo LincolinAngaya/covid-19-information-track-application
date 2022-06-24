8faf6a0158bc7c9ccce2bf4af1892900ad4b8874
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-24 13:06:59 +0300

    debuggging error

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-// //declare variables
-// const countryList = document.querySelector('#countries');
-// const countryName = document.getElementById('country');
-// const countryCode = document.getElementById('code');
-// const date = document.getElementById('date');
-// const confirmed = document.getElementById('confirmed');
-// const deaths = document.getElementById('deaths');
-// const recovered = document.getElementById('recovered');
+//declare variables
+const countryList = document.querySelector('#countries');
+const countryName = document.getElementById('country');
+const countryCode = document.getElementById('code');
+const date = document.getElementById('date');
+const confirmed = document.getElementById('confirmed');
+const deaths = document.getElementById('deaths');
+const recovered = document.getElementById('recovered');
 
-// //add fectch section
-// // document.addEventListener("DOMContentLoaded", () => {
-// //     fetchCountry();
-// // });
+//add fectch section
+document.addEventListener("DOMContentLoaded", () => {
+    fetchCountry();
+});
 
-// //fetch data function
-// function fetchCountry() {
-//     fetch('https://api.covid19api.com/summary')
-//         .then(response => response.json())
-//         .then((county) => renderCountry(county))
-// }
-// renderCountry(county) {
-//     county.forEach(county => {
-//             let list = document.createElement('li');
-//             list.innerText = county.country;
-//             countryList.appendChild(list);
+//fetch data function
+function fetchCountry() {
+    fetch('https://api.covid19api.com/summary')
+        .then(response => response.json())
+        .then((county) => renderCountry(county))
+}
+renderCountry(county) {
+    county.forEach(county => {
+            let list = document.createElement('li');
+            list.innerText = county.Country;
+            countryList.appendChild(list);
 
+            console.log(list)
+                //render spefic country details
 
-//             //render spefic country details
-
-//             list.addEventListener('click', () => {
-
-
-//                 countryName.textContent = county.Country;
-//                 countryCode.textContent = county.CountryCode;
-//                 date.textContent = county.Date;
-//                 confirmed.textContent = county.NewConfirmed;
-//                 deaths.textContent = county.TotalDeaths;
-//                 recovered.textContent = county.TotalRecovered;
-//             })
-
-//         }
+            list.addEventListener('click', () => {
 
 
-//     )
-// }
+                countryName.textContent = county.Country;
+                countryCode.textContent = county.CountryCode;
+                date.textContent = county.Date;
+                confirmed.textContent = county.NewConfirmed;
+                deaths.textContent = county.TotalDeaths;
+                recovered.textContent = county.TotalRecovered;
+            })
+
+        }
+
+
+    )
+}
